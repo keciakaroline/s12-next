fdab5288a4d44484e7e0f825bc249658b7909fce
kecia.karoline <kecia.karoline@codeminer42.com>
2024-04-02 16:20:09 -0300

    build: Add new files and update build manifest

--- .next/static/development/_buildManifest.js
+++ .next/static/development/_buildManifest.js
@@ -1,1 +1,1 @@
-self.__BUILD_MANIFEST = {__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},sortedPages:["\u002F_app"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
+self.__BUILD_MANIFEST = {__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/inventory":["static\u002Fchunks\u002Fpages\u002Finventory.js"],"/rooms":["static\u002Fchunks\u002Fpages\u002Frooms.js"],sortedPages:["\u002F_app","\u002F_error","\u002Finventory","\u002Frooms"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
