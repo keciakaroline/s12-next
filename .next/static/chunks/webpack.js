47909dd3b9e88eaf71d4492ed163ee3fc8eb0afa
kecia.karoline <kecia.karoline@codeminer42.com>
2024-04-02 18:04:30 -0300

    Update .gitignore and page routes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "057c9380b3d4fdd8"; }
+/******/ 		__webpack_require__.h = function() { return "c97e41139d35175d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
