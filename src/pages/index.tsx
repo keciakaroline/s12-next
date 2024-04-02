25a770f2a5b705772afaad07f1e44e58b3085644
kecia.karoline <kecia.karoline@codeminer42.com>
2024-04-02 19:17:18 -0300

    feat: Add router navigation to room 1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,14 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/rooms/1");
+  }, [router]);
+
   return (
     <>
       <Head>
