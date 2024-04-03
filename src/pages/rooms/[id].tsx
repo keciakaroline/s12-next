1dd6f9159485c70895339f99b06ceaf0f049a1bc
kecia.karoline <kecia.karoline@codeminer42.com>
2024-04-03 15:38:43 -0300

    refactor: add typescript

--- src/pages/rooms/[id].tsx
+++ src/pages/rooms/[id].tsx
@@ -1,8 +1,9 @@
 import { useReservations } from "../../application/hooks/useReservations";
 import { Calendar } from "../../ui/components/Calendar";
-import cx from "./index.module.scss";
+import cx from "./Rooms.module.scss";
 import { filterReservationsByRoom } from "../../domain/filterReservationsByRoom";
 import { useParams, useRouter } from "next/navigation";
+import type { RoomType, Reservation } from "../../types/rooms";
 
 const Room = () => {
   const { reservations, rooms } = useReservations();
@@ -15,27 +16,29 @@ const Room = () => {
 
   const [firstRoom] = rooms;
 
-  if (!rooms.some((room) => room.id.toString() === params.id)) {
+  if (!rooms.some((room: RoomType) => room.id.toString() === params.id)) {
     router.replace(`/rooms/${firstRoom.id}`);
     return;
   }
 
-  const selectedRoomId = params.id;
+  const selectedRoomId = params.id as string;
   const selectedRoom = rooms.find(
-    (room) => room.id.toString() === selectedRoomId
+    (room: RoomType) => room.id.toString() === selectedRoomId
   );
   const selectedRoomReservations = filterReservationsByRoom(
     reservations,
     selectedRoom
   );
 
-  const calendarEntries = selectedRoomReservations.map((reservation) => ({
-    id: reservation.id.toString(),
-    title: reservation.student.name,
-    dateStart: reservation.startDate,
-    dateEnd: reservation.endDate,
-    group: reservation.id.toString(),
-  }));
+  const calendarEntries = selectedRoomReservations.map(
+    (reservation: Reservation) => ({
+      id: reservation.id.toString(),
+      title: reservation.student.name,
+      dateStart: reservation.startDate,
+      dateEnd: reservation.endDate,
+      group: reservation.id.toString(),
+    })
+  );
 
   return (
     <>
@@ -46,7 +49,7 @@ const Room = () => {
             router.push(`/rooms/${Number(event.target.value)}`)
           }
         >
-          {rooms.map((room) => (
+          {rooms.map((room: RoomType) => (
             <option
               key={room.id}
               value={room.id}
