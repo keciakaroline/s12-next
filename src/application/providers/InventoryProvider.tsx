1b2e9828c7b842ab7821db74363b754132432ece
kecia.karoline <kecia.karoline@codeminer42.com>
2024-04-03 16:50:19 -0300

    feat: Add useInventory hook and InventoryProvider component

--- src/application/providers/InventoryProvider.tsx
+++ src/application/providers/InventoryProvider.tsx
@@ -1,11 +1,15 @@
 import { createContext, useMemo } from "react";
 import { useEffect, useState } from "react";
 import { fetchInventory } from "../../infrastructure/inner/fetchInventory";
+import type { InventoryItem, FragilityLevel } from "../../types/types";
+import { ReactNode } from "react";
 
-export const InventoryContext = createContext(undefined);
+export const InventoryContext = createContext<
+  { inventory: InventoryItem[] } | undefined
+>(undefined);
 
-export const InventoryProvider = ({ children }) => {
-  const [inventory, setInventory] = useState(undefined);
+export const InventoryProvider = ({ children }: { children: ReactNode }) => {
+  const [inventory, setInventory] = useState<InventoryItem[]>([]);
 
   useEffect(() => {
     fetchInventory().then((data) => setInventory(data));
