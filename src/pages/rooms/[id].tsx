26f49aa933173f40f9fdb1dbe771ac29728df1b2
kecia.karoline <kecia.karoline@codeminer42.com>
2024-04-04 11:09:19 -0300

    feat: Add getServerSideProps and update Room component

--- src/pages/rooms/[id].tsx
+++ src/pages/rooms/[id].tsx
@@ -3,12 +3,41 @@ import { Calendar } from "../../ui/components/Calendar";
 import cx from "./Rooms.module.scss";
 import { filterReservationsByRoom } from "../../domain/filterReservationsByRoom";
 import { useParams, useRouter } from "next/navigation";
-import { Reservation } from "@/types/types";
+import { Reservation, SerializedReservation, Room } from "@/types/types";
+import { GetServerSideProps } from "next";
+import { fetchReservations } from "@/infrastructure/inner/fetchReservations";
+import { getRoomsFromReservations } from "@/utils/getRoomsFromReservations";
 
-const Room = () => {
-  const { reservations, rooms } = useReservations();
+export const getServerSideProps: GetServerSideProps = async () => {
+  const reservations: Reservation[] = await fetchReservations();
+  const rooms = getRoomsFromReservations(reservations);
+  const serializedReservations: SerializedReservation[] = reservations.map(
+    (reservation) => ({
+      ...reservation,
+      startDate: reservation.startDate.toISOString(),
+      endDate: reservation.endDate.toISOString(),
+    })
+  );
+
+  return {
+    props: { rooms, serializedReservations },
+  };
+};
+
+type RoomProps = {
+  serializedReservations: SerializedReservation[];
+  rooms: Room[];
+};
+
+const Room = ({ serializedReservations, rooms }: RoomProps) => {
   const router = useRouter();
   const params = useParams();
+
+  const reservations = serializedReservations.map((reservation) => ({
+    ...reservation,
+    startDate: new Date(reservation.startDate),
+    endDate: new Date(reservation.endDate),
+  }));
 
   if (!reservations || !rooms || rooms.length === 0) {
     return <>Loading...</>;
