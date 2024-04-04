35d665275c7524d97e54c2403032872124f12515
kecia.karoline <kecia.karoline@codeminer42.com>
2024-04-04 11:36:57 -0300

    Refactor: rooms page to use server-side rendering for reservations

--- src/pages/rooms/index.tsx
+++ src/pages/rooms/index.tsx
@@ -1,26 +1,22 @@
-import { useReservations } from "@/application/hooks/useReservations";
-import { useRouter } from "next/navigation";
-import { useEffect, useRef } from "react";
+import { GetServerSideProps } from "next";
+import type { Reservation } from "@/types/types";
+import { fetchReservations } from "@/infrastructure/inner/fetchReservations";
+import { getRoomsFromReservations } from "@/utils/getRoomsFromReservations";
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const reservations: Reservation[] = await fetchReservations();
+  const [firstRoom] = getRoomsFromReservations(reservations);
+
+  return {
+    redirect: {
+      destination: `/rooms/${firstRoom.id}`,
+      permanent: false,
+    },
+  };
+};
 
 const Rooms = () => {
-  const { rooms, isLoading } = useReservations();
-  const router = useRouter();
-
-  const initialize = (isLoading: boolean) => {
-    if (isLoading || !rooms) {
-      return;
-    }
-
-    const [firstRoom] = rooms;
-    router.replace(`/rooms/${firstRoom.id}`);
-  };
-
-  const initializeRef = useRef(initialize);
-  initializeRef.current = initialize;
-
-  useEffect(() => {
-    initializeRef.current(isLoading);
-  }, [isLoading]);
+  return <></>;
 };
 
 export default Rooms;
