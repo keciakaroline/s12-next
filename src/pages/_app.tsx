b9536570a4fd87f0bc2921493a75c9a2b40161e1
kecia.karoline <kecia.karoline@codeminer42.com>
2024-04-04 16:35:22 -0300

    refactor: Remove unused code related to inventory management

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,13 @@
 import type { AppProps } from "next/app";
 import { Header } from "../ui/components/Header";
-import { InventoryProvider } from "../application/providers/InventoryProvider";
 
 import "../ui/styles/global.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <InventoryProvider>
+    <>
       <Header />
       <Component {...pageProps} />
-    </InventoryProvider>
+    </>
   );
 }
